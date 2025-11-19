a2a2e9106b99b981b96438144a2e08d9d7dd184c
TheJoing <lucassartor100@hotmail.com>
2025-11-19 19:17:03 -0300

    lucas

--- script1.R
+++ script1.R
@@ -1,6 +1,20 @@
 library(tidyverse)
 
-dados <- read.csv2("dados_ml.csv")
+dados <- read.csv2("dados_ml.csv") 
+  
+dados$dias <- yday(dados$Epoca_de_semeadura)
 
+teste <- dados %>% 
+  mutate(
+    prod = case_when(
+      Produtividade <= quantile(Produtividade, 0.25)  ~ "Baixa",
+      Produtividade <= quantile(Produtividade, 0.5)  ~ "Media",
+      Produtividade <= quantile(Produtividade, 0.75)  ~ "Alta",
+      TRUE      ~ "Altissima"
+    )
+  ) %>% 
+  select(-c(COD_PROD, Cod_Estacao_Met, Area_colhida, Epoca_de_semeadura, Cultivar)) %>%
+  drop_na()
 
-View(dados)
+# summary(dados$Produtividade)
+View(teste)
